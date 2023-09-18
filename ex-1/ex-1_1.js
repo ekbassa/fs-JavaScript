abb83cb534dc72c389536d553e6798e099d82f58
sambar <bassameketeh@gmail.com>
2023-09-18 12:35:30 +0300

    adding ex-1.1

---
+++ ex-1/ex-1_1.js
@@ -0,0 +1,7 @@
+let sTudentName = 'Mark;' // string 
+let age = 28; // number
+let someThing = null  // object
+let angry = false;  // boolean
+let cart = null // 
+let customerId   // Undefined
+let symbol = '!';
