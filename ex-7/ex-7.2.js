288a8703132bc8285f3fd46a063447f0ecee1fa0
sambar <bassameketeh@gmail.com>
2023-09-19 12:16:09 +0300

    adding ex-7.2.js

---
+++ ex-7/ex-7.2.js
@@ -0,0 +1,6 @@
+const string = "The more you know, the more you know that you don't know";
+const searchTerm = `you`;
+
+const index = string.lastIndexOf(searchTerm);
+console.log(`the last index of the word \"you\" is on index: ${index}`);
+
