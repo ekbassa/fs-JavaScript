c3e75d63629d862b214ca78423659239a98079b2
sambar <bassameketeh@gmail.com>
2023-09-27 10:36:46 +0300

    fixing- adding the else statmenet for  ex-10.1.js

--- ex-10/ex-10.1.js
+++ ex-10/ex-10.1.js
@@ -2,6 +2,9 @@ function isString(str, callback) {
   if (typeof str === "string") {
     callback(str);
   }
+  else{
+    console.log(`${str}: Is not a string!`)
+  }
 }
 
 function myString(str) {
@@ -9,3 +12,4 @@ function myString(str) {
 }
 
 isString("hello from callback!", myString);
+// isString(123, myString);
