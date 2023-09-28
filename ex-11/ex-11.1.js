2c0877cd6b76cddf74caec54a7618866cdfd3d7e
sambar <bassameketeh@gmail.com>
2023-09-28 13:28:59 +0300

    adding ex-11.1.js

---
+++ ex-11/ex-11.1.js
@@ -0,0 +1,50 @@
+const newReleases = [
+  {
+    id: 70111470,
+    title: "Die Hard",
+    boxart: "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+    uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+    rating: [4.0],
+    bookmark: [],
+  },
+  {
+    id: 654356453,
+    title: "Bad Boys",
+    boxart: "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+    uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+    rating: [5.0],
+    bookmark: [{ id: 432534, time: 65876586 }],
+  },
+  {
+    id: 65432445,
+    title: "The Chamber",
+    boxart: "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+    uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+    rating: [4.0],
+    bookmark: [],
+  },
+  {
+    id: 675465,
+    title: "Fracture",
+    boxart: "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+    uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+    rating: [5.0],
+    bookmark: [{ id: 432534, time: 65876586 }],
+  },
+];
+
+function arrTitleAndId(movies) {
+    const result = [];
+
+    movies.forEach((movie)=>{
+        // I am using Destructuring to extract the object properties and values
+
+        const {id,title} = movie;
+        result.push({id,title})
+        
+    });
+    return result
+}
+
+const res = arrTitleAndId(newReleases);
+console.log(res);
