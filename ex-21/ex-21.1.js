8660c4621167a96a0f83167217f6d55c3314c94c
sambar <bassameketeh@gmail.com>
2023-10-03 10:58:53 +0300

    adding ex-21.js file

---
+++ ex-21/ex-21.1.js
@@ -0,0 +1,12 @@
+const ageInput = document.querySelector('#age');
+const checkAge = document.getElementById('check-age');
+
+checkAge.addEventListener('click',function(){
+    const age = parseInt(ageInput.value);
+    if (age > 18 ){
+        alert('“you can drink ')
+    }
+    else{
+        alert('you’re too young')
+    }
+})
